516574982068b58121365ca6e1d38cdc100bfcfd
dngthanhduy258 <dngthanhduy258@gmail.com>
2024-06-27 09:29:11 +0700

    Done bài thi

--- ExamWeb_DuongThanhDuy/Models/DiaNhac.cs
+++ ExamWeb_DuongThanhDuy/Models/DiaNhac.cs
@@ -10,12 +10,17 @@ namespace ExamWeb_DuongThanhDuy.Models
     {
         public int Id { get; set; }
         [StringLength(250)]
+        [Required(ErrorMessage ="Chưa nhập tên cd")]
         public string TuaCD { get; set; }
         [StringLength(50)]
+        [Required(ErrorMessage = "Chưa nhập tên nghệ sĩ")]
 
         public string NgheSi { get; set; }
         public bool TrongNuoc { get; set; }
+        [Range(1,1000,ErrorMessage ="Số lượng không hợp lệ")]
         public double GiaBan { get; set; }
+        [Range(1, 1000, ErrorMessage = "Số lượng không hợp lệ")]
+
         public int SoLuong { get; set; }
     }
 }
